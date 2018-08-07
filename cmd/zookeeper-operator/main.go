e0bb156061ba41b27cf7c28dd5478b69a4f66241
David Maddison <david.maddison@emc.com>
2018-08-07 12:28:26 +0100

    Issue 11: Allow blank Watch Namespace

--- cmd/zookeeper-operator/main.go
+++ cmd/zookeeper-operator/main.go
@@ -9,7 +9,9 @@ import (
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	stub "github.com/pravega/zookeeper-operator/pkg/stub"
 
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func printVersion() {
@@ -23,7 +25,7 @@ func main() {
 
 	resource := "zookeeper.pravega.io/v1beta1"
 	kind := "ZookeeperCluster"
-	namespace, err := k8sutil.GetWatchNamespace()
+	namespace, err := getWatchNamespaceAllowBlank()
 	if err != nil {
 		logrus.Fatalf("Failed to get watch namespace: %v", err)
 	}
@@ -32,4 +34,13 @@ func main() {
 	sdk.Watch(resource, kind, namespace, resyncPeriod)
 	sdk.Handle(stub.NewHandler())
 	sdk.Run(context.TODO())
+}
+
+// GetWatchNamespaceAllowBlank returns the namespace the operator should be watching for changes
+func getWatchNamespaceAllowBlank() (string, error) {
+	ns, found := os.LookupEnv(k8sutil.WatchNamespaceEnvVar)
+	if !found {
+		return "", fmt.Errorf("%s must be set", k8sutil.WatchNamespaceEnvVar)
+	}
+	return ns, nil
 }
