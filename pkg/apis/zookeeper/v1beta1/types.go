9e2fcd9cc27aaa17d0f0cb7931276369ea99ff1b
Adrian Moreno <adrian@morenomartinez.com>
2018-09-25 15:25:47 +0200

    Add License file and license header to Go files

--- pkg/apis/zookeeper/v1beta1/types.go
+++ pkg/apis/zookeeper/v1beta1/types.go
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package v1beta1
 
 import (
@@ -276,6 +286,6 @@ type ClusterStatus struct {
 // MembersStatus is the status of the members of the cluster with both
 // ready and unready node membership lists
 type MembersStatus struct {
-	Ready []string `json:"ready"`
+	Ready   []string `json:"ready"`
 	Unready []string `json:"unready"`
 }
