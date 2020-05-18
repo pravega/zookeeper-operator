1f2615578c92dbe33c1ecc9fdeac694220403d50
prabhaker24 <prabhaker.saxena@dell.com>
2020-05-18 15:41:41 +0530

    code coverage increased
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

--- pkg/apis/zookeeper/v1beta1/status_test.go
+++ pkg/apis/zookeeper/v1beta1/status_test.go
@@ -50,7 +50,44 @@ var _ = Describe("ZookeeperCluster Status", func() {
 		})
 	})
 
-	Context("set conditions", func() {
+	Context("manually set pods upgrade condition to be true", func() {
+		BeforeEach(func() {
+			condition := v1beta1.ClusterCondition{
+				Type:               v1beta1.ClusterConditionUpgrading,
+				Status:             corev1.ConditionTrue,
+				Reason:             "",
+				Message:            "",
+				LastUpdateTime:     "",
+				LastTransitionTime: "",
+			}
+			z.Status.Conditions = append(z.Status.Conditions, condition)
+		})
+
+		It("should contains pods upgrade condition and it is true status", func() {
+			_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionUpgrading)
+			Ω(condition.Status).To(Equal(corev1.ConditionTrue))
+		})
+	})
+	Context("manually set pods Error condition to be true", func() {
+		BeforeEach(func() {
+			condition := v1beta1.ClusterCondition{
+				Type:               v1beta1.ClusterConditionError,
+				Status:             corev1.ConditionTrue,
+				Reason:             "",
+				Message:            "",
+				LastUpdateTime:     "",
+				LastTransitionTime: "",
+			}
+			z.Status.Conditions = append(z.Status.Conditions, condition)
+		})
+
+		It("should contains pods upgrade condition and it is true status", func() {
+			_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionError)
+			Ω(condition.Status).To(Equal(corev1.ConditionTrue))
+		})
+	})
+
+	Context("set conditions for pods ready", func() {
 		Context("set pods ready condition to be true", func() {
 			BeforeEach(func() {
 				z.Status.SetPodsReadyConditionFalse()
@@ -81,4 +118,69 @@ var _ = Describe("ZookeeperCluster Status", func() {
 			})
 		})
 	})
+
+	Context("set conditions for upgrade", func() {
+		Context("set pods upgrade condition to be true", func() {
+			BeforeEach(func() {
+				z.Status.SetUpgradingConditionFalse()
+				z.Status.SetUpgradingConditionTrue(" ", " ")
+			})
+			It("should have pods upgrade condition with true status", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionUpgrading)
+				Ω(condition.Status).To(Equal(corev1.ConditionTrue))
+			})
+		})
+
+		Context("set pod upgrade condition to be false", func() {
+			BeforeEach(func() {
+				z.Status.SetUpgradingConditionTrue(" ", " ")
+				z.Status.SetUpgradingConditionFalse()
+			})
+
+			It("should have upgrade condition with false status", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionUpgrading)
+				Ω(condition.Status).To(Equal(corev1.ConditionFalse))
+			})
+
+			It("should have updated timestamps", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionUpgrading)
+				// TODO: check the timestamps
+				Ω(condition.LastUpdateTime).NotTo(Equal(""))
+				Ω(condition.LastTransitionTime).NotTo(Equal(""))
+			})
+		})
+	})
+
+	Context("set conditions for Error", func() {
+		Context("set pods Error condition to be true", func() {
+			BeforeEach(func() {
+				z.Status.SetErrorConditionFalse()
+				z.Status.SetErrorConditionTrue(" ", " ")
+			})
+			It("should have pods Error condition with true status", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionError)
+				Ω(condition.Status).To(Equal(corev1.ConditionTrue))
+			})
+		})
+
+		Context("set pod Error condition to be false", func() {
+			BeforeEach(func() {
+				z.Status.SetErrorConditionTrue(" ", " ")
+				z.Status.SetErrorConditionFalse()
+			})
+
+			It("should have Error condition with false status", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionError)
+				Ω(condition.Status).To(Equal(corev1.ConditionFalse))
+			})
+
+			It("should have updated timestamps", func() {
+				_, condition := z.Status.GetClusterCondition(v1beta1.ClusterConditionError)
+				// TODO: check the timestamps
+				Ω(condition.LastUpdateTime).NotTo(Equal(""))
+				Ω(condition.LastTransitionTime).NotTo(Equal(""))
+			})
+		})
+	})
+
 })
