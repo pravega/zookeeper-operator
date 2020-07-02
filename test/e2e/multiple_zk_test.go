d46a127435d06a91c6cfa386683ae26f6777758f
prabhaker24 <prabhaker.saxena@dell.com>
2020-07-02 17:23:18 +0530

    aaded back all the tests
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

--- test/e2e/multiple_zk_test.go
+++ test/e2e/multiple_zk_test.go
@@ -113,6 +113,10 @@ func testMultiZKCluster(t *testing.T) {
 	err = zk_e2eutil.WaitForClusterToBecomeReady(t, f, ctx, zk1, podSize)
 	g.Expect(err).NotTo(HaveOccurred())
 
+	// This is to get the latest Zookeeper cluster object
+	zk2, err = zk_e2eutil.GetCluster(t, f, ctx, zk2)
+	g.Expect(err).NotTo(HaveOccurred())
+
 	//upgrade the image in second Cluster
 	zk2.Spec.Image.Tag = upgradeVersion
 
